1fb930bc62103a502000f253b41d75749b1b70af
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2019-12-22 01:48:29 +0200

    [CardmarketUserProductsHighlighting] Refactoring to adapt for new user products page design.

--- CardmarketUserProductsHighlighting.user.js
+++ CardmarketUserProductsHighlighting.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Highlights products in user product browser according to price (and other parameters).
 // @author       vsmoustakas@gmail.com
-// @match        https://www.cardmarket.com/en/Magic/MainPage/browseUserProducts?idCategory=1&idUser=*
+// @match        https://www.cardmarket.com/en/Magic/Users/*/Offers/*?*
 // @require      http://code.jquery.com/jquery-3.4.1.min.js
 // @grant        GM_addStyle
 // ==/UserScript==
@@ -26,28 +26,28 @@ function cleanPriceStr(priceStr) {
 }
 
 $( document ).ready(function() {
-    alert("Ready?");
-//    var name = $(document).find('.MKMTable.MKMSortable.fullWidth').find('> tbody > tr').each.find("td:eq(2)");
-//    console.log(name.text());
-//   var links = name.find("div > div > a");
-//    console.log(links.href);
+    alert('Ready?');
 
-    $(document).find('.MKMTable.MKMSortable.fullWidth').find('> tbody > tr').each(function() {
-        var productName = jQuery(this).find("td:eq(2)").text();
-        console.log(productName);
-        var productPageURL = jQuery(this).find("td:eq(2) > div > div > a")[0].href;
-        console.log(productPageURL);
-        // Try getting getting the PPU price first
-        var sellerProductPriceStr = cleanPPUStr(jQuery(this).find("td:eq(9) > div > .microFont.c-k6.f-w400.f-sIta.nowrap").text());
-        // If empty get the normal price
-        if (sellerProductPriceStr === "") {
-            sellerProductPriceStr = cleanPriceStr(jQuery(this).find("td:eq(9) > div > .algn-r.nowrap").text());
+    $(document).find('div.table.article-table.table-striped').find('> div.table-body > div').each(function() {
+        var product = jQuery(this).find('> div.col-sellerProductInfo.col > div.row.no-gutters > div.col-seller.col-12.col-md-auto');
+        var productName = product.text();
+        var productPageURL = product.find('> a')[0].href;
+
+        // Offer price (or playset PPU)
+        var offer = jQuery(this).find('> div.col-offer > div.price-container.d-none.d-md-flex.justify-content-end > div.d-flex.flex-column');
+        var offerPrice = offer.find('> div.d-flex.align-items-center.justify-content-end');
+        var offerPricePlaysetPPU = offer.find('> span.extra-small.font-italic.text-muted.text-nowrap.d-none.d-md-inline');
+
+        var offerPriceStr = cleanPriceStr(offerPrice.text());
+        if (offerPricePlaysetPPU.length !== 0) {
+            offerPriceStr = cleanPPUStr(offerPricePlaysetPPU.text());
         }
-        var sellerProductPriceFloat = parseFloat(sellerProductPriceStr);
-        console.log(sellerProductPriceFloat);
+        var offerPriceFloat = parseFloat(offerPriceStr);
+        //console.log(offerPriceFloat);
 
         var productPageHTML = getRemote(productPageURL);
-        console.log(productName + '\'s info page HTML: ' + productPageHTML);
+        console.log(productName + ' @ ' + productPageURL);
+        console.log(productPageHTML);
         var productInfo = $(productPageHTML).find('.info-list-container.col-12.col-md-8.col-lg-12.mx-auto.align-self-start');
         var productInfoData = productInfo.find(".labeled.row.no-gutters.mx-auto");
         var productPriceFromFloat = parseFloat(cleanPriceStr(productInfoData.find("dt:contains('From')").next().text()));
@@ -55,54 +55,49 @@ $( document ).ready(function() {
         var productPriceAvg30Float = parseFloat(cleanPriceStr(productInfoData.find("dt:contains('30-days average price')").next().text()));
         var productPriceAvg7Float = parseFloat(cleanPriceStr(productInfoData.find("dt:contains('7-days average price')").next().text()));
         var productPriceAvg1Float = parseFloat(cleanPriceStr(productInfoData.find("dt:contains('1-day average price')").next().text()));
-        console.log('From:' + productPriceFromFloat + '|Trend:' + productPriceTrendFloat + '|Avg30:' + productPriceAvg30Float + '|Avg7:' + productPriceAvg7Float + '|Avg1:' + productPriceAvg1Float);
+        //console.log('From:' + productPriceFromFloat + '|Trend:' + productPriceTrendFloat + '|Avg30:' + productPriceAvg30Float + '|Avg7:' + productPriceAvg7Float + '|Avg1:' + productPriceAvg1Float);
 
-        var totalItemsCount = 0;
-        var totalItemsSumPriceFloat = 0.0;
-        var itemsAvgPriceFloat = 0.0;
-        var itemsMinPriceFloat = Number.MAX_VALUE;
+        var productPageOffersCount = 0;
+        var productPageOffersPriceSumFloat = 0.0;
+        var productPageOffersPriceAvgFloat = 0.0;
+        var productPageOffersPriceMinFloat = Number.MAX_VALUE;
         $(productPageHTML).find('.table-body > .row.no-gutters.article-row').each(function () {
-            var offer = $(this).find(".col-offer");
-            var offerPrice = $(offer).find(".price-container.d-none.d-md-flex.justify-content-end");
-            //console.log(offerPrice);
-            // Try getting getting the PPU price first
-            var offerItemPriceStr = cleanPPUStr($(offerPrice).find(".extra-small.font-italic.text-muted.text-nowrap.d-none.d-md-inline").text());
-            //console.log(offerItemPriceStr);
-            // If empty get the normal price
-            if (offerItemPriceStr === "" ) {
-                offerItemPriceStr = cleanPriceStr($(offerPrice).find(".font-weight-bold.color-primary.small.text-right.text-nowrap").text());
-            }
-            //console.log(offerItemPriceStr);
-            var offerItemPriceFloat = parseFloat(offerItemPriceStr);
-            itemsMinPriceFloat = Math.min(itemsMinPriceFloat, offerItemPriceFloat)
-            var offerItemsCount = parseInt($(offer).find('.amount-container.d-none.d-md-flex.justify-content-end.mr-3').text());
-            var offerItemsSumPriceFloat = offerItemsCount * offerItemPriceFloat;
-            console.log('Offer: ' + offerItemsCount + ' x ' + offerItemPriceFloat + ' = ' + offerItemsSumPriceFloat);
-            totalItemsCount += offerItemsCount;
-            totalItemsSumPriceFloat += offerItemsSumPriceFloat;
-        });
-        itemsAvgPriceFloat = totalItemsSumPriceFloat/totalItemsCount;
-        console.log('count: ' + totalItemsCount + '| sum: ' + totalItemsSumPriceFloat + ' | avg: ' + itemsAvgPriceFloat + ' | min price: ' + itemsMinPriceFloat);
+            // Offer price (or playset PPU)
+            var productPageOffer = jQuery(this).find('> div.col-offer');
+            var productPageOfferPrices = productPageOffer.find('> div.price-container.d-none.d-md-flex.justify-content-end > div.d-flex.flex-column');
+            var productPageOfferPrice = productPageOfferPrices.find('> div.d-flex.align-items-center.justify-content-end');
+            var productPageOfferPricePlaysetPPU = productPageOfferPrices.find('> span.extra-small.font-italic.text-muted.text-nowrap.d-none.d-md-inline');
+            var productPageOfferItemsCount = parseInt(productPageOffer.find('> div.amount-container.d-none.d-md-flex.justify-content-end.mr-3').text());
 
-        if (sellerProductPriceFloat <= itemsMinPriceFloat) {
+            var productPageOfferPriceStr = cleanPriceStr(productPageOfferPrice.text());
+            if (productPageOfferPricePlaysetPPU.length !== 0) {
+                productPageOfferPriceStr = cleanPPUStr(productPageOfferPricePlaysetPPU.text());
+                productPageOfferItemsCount = 4 * productPageOfferItemsCount;
+            }
+            var productPageOfferPriceFloat = parseFloat(productPageOfferPriceStr);
+            //console.log(productPageOfferPriceFloat);
+
+            productPageOffersPriceMinFloat = Math.min(productPageOffersPriceMinFloat, productPageOfferPriceFloat);
+            var productPageOfferItemsPriceSumFloat = productPageOfferItemsCount * productPageOfferPriceFloat;
+            //console.log('Offer: ' + productPageOfferItemsCount + ' x ' + productPageOfferPriceFloat + ' = ' + productPageOfferItemsPriceSumFloat);
+            productPageOffersCount += productPageOfferItemsCount;
+            productPageOffersPriceSumFloat += productPageOfferItemsPriceSumFloat;
+        });
+        productPageOffersPriceAvgFloat = productPageOffersPriceSumFloat/productPageOffersCount;
+        //console.log('count: ' + productPageOffersCount + '| sum: ' + productPageOffersPriceSumFloat + ' | avg: ' + productPageOffersPriceAvgFloat + ' | min price: ' + productPageOffersPriceMinFloat);
+
+        if (offerPriceFloat <= productPageOffersPriceMinFloat) {
             jQuery(this).css("background-color", "limegreen");
-        } else if (sellerProductPriceFloat <= itemsAvgPriceFloat) {
+        } else if (offerPriceFloat <= productPageOffersPriceAvgFloat) {
             jQuery(this).css("background-color", "lime");
-        } else if (sellerProductPriceFloat <= productPriceAvg30Float) {
+        } else if (offerPriceFloat <= productPriceAvg30Float) {
             jQuery(this).css("background-color", "greenyellow");
-        } else if (sellerProductPriceFloat > productPriceFromFloat && sellerProductPriceFloat > productPriceTrendFloat && sellerProductPriceFloat > productPriceAvg30Float && sellerProductPriceFloat > productPriceAvg7Float && sellerProductPriceFloat > productPriceAvg1Float){
+        } else if (offerPriceFloat > productPriceFromFloat && offerPriceFloat > productPriceTrendFloat && offerPriceFloat > productPriceAvg30Float && offerPriceFloat > productPriceAvg7Float && offerPriceFloat > productPriceAvg1Float){
             jQuery(this).css("background-color", "red");
         } else {
             jQuery(this).css("background-color", "yellow");
         }
     });
-
-//    $(document).find('.MKMTable.MKMSortable.fullWidth').find('> tbody > tr').each(function() {
-//        var name = jQuery(this).find("td:eq(2)")
-//        var link = name.add(".vAlignMiddle.dualTextDiv div a");
-//        console.log(name.text());
-//        console.log(link)
-//        });
 });
 
 (function() {
